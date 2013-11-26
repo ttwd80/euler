c3c32fc33d942c79223014d2e819346d230186a2
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-11-26 10:05:15 +0800

    doable in js

--- src/main/webapp/resources/solution/001.js
+++ src/main/webapp/resources/solution/001.js
@@ -1,6 +1,20 @@
 $(function() {
 	$("#form").submit(function(event) {
-		alert('x');
+		var a = parseInt($("#a").val());
+		var b = parseInt($("#b").val());
+		var c = parseInt($("#c").val());
+		var answer = solve(a, b, c);
+		$("#answer").val(answer);
 		event.preventDefault();
 	});
+	function solve(a, b, c) {
+		var sum = 0;
+		var i;
+		for (i = 1; i < c; i++) {
+			if (i % a == 0 || i % b == 0) {
+				sum = sum + i;
+			}
+		}
+		return sum;
+	}
 });
